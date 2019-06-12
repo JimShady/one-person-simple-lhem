bb8c58858cac3bbb62f1214b178880174fe98c58
James <james.david.smith@gmail.com>
2019-06-12 11:19:54 +0100

    Create 2-get_cmaq_concentrations.R

---
+++ 2-get_cmaq_concentrations.R
@@ -0,0 +1,13 @@
+## Go through each of the day tables
+
+## Smoething like.... in a loop ....
+
+## Import table
+## Turn to spatial points data frame
+## Look at the date the day is for
+## download from C3 the correct netcdf files (define pollutants here?)
+## Format/turn netcdf files into raster files
+## Extract concentrations, by hour, from the netcdf files
+## store back in the day tables
+## write day tables back to a file. Suggest new file and folder i.e
+## 'day_tables_plus_pollutants/345345344.Rdata'
